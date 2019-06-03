e6406c13ace921bf88f5ac3cb5f86cedce9e3aba
Deep Doshi <deepdoshi@live.in>
2019-06-02 18:48:59 -0700

    feat: add RAM percent function

--- ProcessParser.h
+++ ProcessParser.h
@@ -253,6 +253,50 @@ float ProcessParser::getSysIdleCpuTime(vector<string> values) {
     return (stof(values[S_IDLE]) + stod(values[S_IOWAIT]));
 }
 
+float ProcessParser::getSysRamPercent() {
+    string line;
+    string name1 = "MemAvailble:";
+    string name2 = "MemFree:";
+    string name3 = "Buffers:"
+    int result;
+    ifstream stream;
+
+    Util::getStream((Path::basePath() + Path::memInfoPath()), stream);
+
+    float total_mem = 0;
+    float free_mem = 0;
+    float buffers = 0;
+
+    while(getline(stream, line)) {
+        if(total_mem != 0 && free_mem != 0)
+            break;
+        
+        if(line.compare(0, name1.size(), name1) == 0) {
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+            total_mem = stof(values[1]);
+        }
+
+        if (line.compare(0, name2.size(), name2) == 0) {
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+            free_mem = stof(values[1]);
+        }
+
+        if (line.compare(0, name3.size(), name3) == 0) {
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+            buffers = stof(values[1]);
+        }
+    }
+
+    // calculate usage
+    return (float)(100.0 * (1 - (free_mem / (total_mem - buffers))));
+}
+
 int ProcessParser::getNumberOfCores() {
     // Get number of host cpu cores
     string line;
